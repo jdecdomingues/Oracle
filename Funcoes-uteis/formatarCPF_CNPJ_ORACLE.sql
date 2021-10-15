383a6d38a851b04ccd2b6806cb8fca0bacddb830
Jonathan Domingues <56741889+jdecdomingues@users.noreply.github.com>
2021-10-15 09:10:23 -0300

    Atualização

---
+++ Funcoes-uteis/formatarCPF_CNPJ_ORACLE.sql
@@ -0,0 +1,37 @@
+/****************************************
+*****FUNÇÃO PARA FORMATAR CPF E CNPJ*****
+*****    BANCO DE DADOS: ORACLE     *****
+*****************************************/
+
+
+CREATE OR REPLACE FUNCTION formatarCPF_CNPJ(Valor VARCHAR2)
+   RETURN VARCHAR2
+IS
+   RESULT   VARCHAR2 (20);
+BEGIN
+   IF LENGTH (Valor) > 11
+   THEN
+      result :=
+            SUBSTR (Valor, 1, 2)
+         || '.'
+         || SUBSTR (Valor, 3, 3)
+         || '.'
+         || SUBSTR (Valor, 6, 3)
+         || '/'
+         || SUBSTR (Valor, 9, 4)
+         || '-'
+         || SUBSTR (Valor, 13, 2);
+   ELSE
+      result :=
+            SUBSTR (Valor, 1, 3)
+         || '.'
+         || SUBSTR (Valor, 4, 3)
+         || '.'
+         || SUBSTR (Valor, 7, 3)
+         || '-'
+         || SUBSTR (Valor, 10, 2);
+   END IF;
+
+   RETURN (RESULT);
+END;
+/
